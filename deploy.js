8a93af454b295b546cfecc64861d68efa97fcdd6
Rangga <arsyrangga.com>
2023-03-15 11:48:13 +0700

    new inbox

---
+++ deploy.js
@@ -0,0 +1,30 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const Web3 = require('web3')
+const {abi, evm} = require('./compile')
+
+const provider = new HDWalletProvider(
+    'screen abuse cricket furnace type spell episode orphan lend broken toe night',
+    'https://goerli.infura.io/v3/7c932dca64454bf0bbcb46bff08552c1'
+)
+
+
+const web3 = new Web3(provider)
+
+const deploy = async () =>{
+    const accounts = await web3.eth.getAccounts()
+
+    console.log('Attempting to deploy from account', accounts[0])
+
+    const result = await new web3.eth.Contract(abi).deploy({ 
+        data : evm.bytecode.object,
+        arguments : ['Hi There']
+     }).send({
+        gas : '1000000',
+        from : accounts[0]
+     })
+
+     console.log('Contract deployed to', result.options.address)
+     provider.engine.stop()
+}
+
+deploy()
